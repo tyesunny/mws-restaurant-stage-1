c70056bde7eb9763f45b3e62de59749a9bb88246
Tang Ye <tanielye@gmail.com>
2018-01-19 22:53:39 -0500

    feat: a11y: insert alt for <img> tag

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `${restaurant.name} cover photo`;
   li.append(image);
 
   const name = document.createElement('h1');
